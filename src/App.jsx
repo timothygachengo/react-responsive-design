d78331635a7d9e61dd0c0ca0b8313c6056f6db5e
timothymugodev <timomugo01@gmail.com>
2023-05-03 10:10:24 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Home from './Home'
+
+
+
+function App() {
+
+  return (
+    <>
+     <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import testImg from './assets/house.jpg';
+
+const Home = () => {
+    return (
+        <div className="flex justify-between">
+            <div className="flex-auto"></div>
+                <ul className="flex flex-1 flex-col md:flex-row justify-between">
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                    <li>
+                        <a href='#'>Contact</a>
+                    </li>
+                </ul>
+            {/* <img className="cover_img" src={testImg} alt="test" /> */}
+        </div>
+    )
+}
+
+export default Home;
